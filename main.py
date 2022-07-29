785ea0bc1e5a32b669dc598164b39ab9bc349152
Chau (personal) <playerzawesome@gmail.com>
2022-07-29 15:06:40 +0700

    Create and constrain snake movements. Create collision between snake and fruit.

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-import pygame
 import sys
-import random
 
-from objects import Fruit
+import pygame
 
-FPS = 60
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
-BG_COLOR = "#F3E9DD"
+from constants import WINDOW_WIDTH, WINDOW_HEIGHT, BG_COLOR, FPS
+from objects import Fruit, Snake, generate_random_coordinates
 
-FRUIT_WIDTH = 20
+pygame.init()
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Snake")
 
-pygame.init()
-
+# create a clock to govern fps
 clock = pygame.time.Clock()
 
 # create Fruit
-topleft_x = random.randint(0, WINDOW_WIDTH - FRUIT_WIDTH)
-topleft_y = random.randint(0, WINDOW_HEIGHT - FRUIT_WIDTH)
-fruit = Fruit(FRUIT_WIDTH)
+topleft_x, topleft_y = generate_random_coordinates()
+fruit = Fruit(topleft_x, topleft_y)
+
+# create Snake
+snake = Snake()
+
+direction = "down"
 
 running = True
 while running:
@@ -29,10 +28,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and direction != "right":
+                direction = "left"
+            elif event.key == pygame.K_RIGHT and direction != "left":
+                direction = "right"
+            elif event.key == pygame.K_UP and direction != "down":
+                direction = "up"
+            elif event.key == pygame.K_DOWN and direction != "up":
+                direction = "down"
+
+    # fill the display surface
     display_surface.fill(BG_COLOR)
+    snake.blit(display_surface)
+
+    if direction == "left" and snake.head.rect.left >= 0:
+        snake.move_left()
+    elif direction == "right" and snake.head.rect.right <= WINDOW_WIDTH:
+        snake.move_right()
+    elif direction == "up" and snake.head.rect.top >= 0:
+        snake.move_up()
+    elif direction == "down" and snake.head.rect.bottom <= WINDOW_HEIGHT:
+        snake.move_down()
 
     # fruit auto-generate at random position on the display surface
-    fruit.generate(display_surface, topleft_x, topleft_y)
+    if snake.head.rect.colliderect(fruit.rect):
+        fruit.generate()
+
+    fruit.blit(display_surface)
 
     pygame.display.update()
     clock.tick(FPS)
--- objects.py
+++ objects.py
@@ -1,21 +1,57 @@
+import random
+
 import pygame
 
+from constants import WINDOW_WIDTH, WINDOW_HEIGHT, FRUIT_WIDTH, SNAKE_VELOCITY
 
-class Fruit:
-    def __init__(self, width=20):
-        self.width = width
+
+def generate_random_coordinates():
+    topleft_x = random.randint(0, WINDOW_WIDTH - FRUIT_WIDTH)
+    topleft_y = random.randint(0, WINDOW_HEIGHT - FRUIT_WIDTH)
+    return topleft_x, topleft_y
+
+
+class Cube:
+    def __init__(self, topleft_x, topleft_y):
+        self.width = FRUIT_WIDTH
+        self.rect = pygame.Rect(topleft_x, topleft_y, self.width, self.width)
+
+
+class Fruit(Cube):
+    def __init__(self, topleft_x, topleft_y):
+        super().__init__(topleft_x, topleft_y)
         self.fill_color = "#9D5353"
 
-    def generate(self, surface, topleft_x, topleft_y):
+    def generate(self):
         """
         Generate itself at a new random position each time the snake eats it.
-
-        :param topleft_x: the x coordinate of the top left corner.
-        :type topleft_x: int
-        :param topleft_y: the y coordinate of the top left corner.
-        :type topleft_y: int
-        :param surface: the Surface to generate on.
-        :type surface: pygame.Surface
         """
-        fruit_rect = pygame.Rect(topleft_x, topleft_y, self.width, self.width)
-        pygame.draw.rect(surface, self.fill_color, fruit_rect)
+        topleft_x, topleft_y = generate_random_coordinates()
+        self.rect = pygame.Rect(topleft_x, topleft_y, self.width, self.width)
+
+    def blit(self, surface):
+        pygame.draw.rect(surface, self.fill_color, self.rect)
+
+
+class Snake:
+    def __init__(self):
+        topleft_x, topleft_y = generate_random_coordinates()
+        self.head = Cube(topleft_x, topleft_y)
+        self.body = []
+        self.length = 0
+        self.fill_color = "#FF5B00"
+
+    def move_left(self):
+        self.head.rect.centerx -= SNAKE_VELOCITY
+
+    def move_right(self):
+        self.head.rect.centerx += SNAKE_VELOCITY
+
+    def move_up(self):
+        self.head.rect.centery -= SNAKE_VELOCITY
+
+    def move_down(self):
+        self.head.rect.centery += SNAKE_VELOCITY
+
+    def blit(self, surface):
+        pygame.draw.rect(surface, self.fill_color, self.head.rect)
